c9bdd3584563388de84db3bcbe73098f323ba9d4
Daniel Marschall <info@daniel-marschall.de>
2021-04-25 20:06:14 +0000

    Cookie SAMESITE policy can now be configured in the base configuration file. New clas "OIDplusCookieUtils". In shebang, using "env php" instead of "/usr/bin/php".
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@557 02e4d621-2042-4998-a0bc-9ccd24201011

---
+++ includes/classes/OIDplusCookieUtils.class.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * OIDplus 2.0
+ * Copyright 2019 - 2021 Daniel Marschall, ViaThinkSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+if (!defined('INSIDE_OIDPLUS')) die();
+
+class OIDplusCookieUtils {
+
+	public function unsetcookie($name) {
+		$this->setcookie($name, '', time()-9999, true);
+	}
+
+	public function setcookie($name, $value, $expires=0, $allowJS=false) {
+		// $path = ini_get('session.cookie_path');
+		$path = OIDplus::webpath(null,true);
+		if (empty($path)) $path = '/';
+
+		$domain = '';
+		$secure = false;
+		$httponly = !$allowJS;
+		$samesite = OIDplus::baseConfig()->getValue('COOKIE_SAMESITE_POLICY', 'Strict');
+
+		if (strnatcmp(phpversion(),'7.3.0') >= 0) {
+			$options = array(
+				"expires" => $expires,
+				"path" => $path,
+				"domain" => $domain,
+				"secure" => $secure,
+				"httponly" => $httponly,
+				"samesite" => $samesite
+			);
+			setcookie($name, $value, $options);
+		} else {
+			setcookie($name, $value, $expires, $path.'; samesite='.$samesite, $domain, $secure, $httponly);
+		}
+	}
+
+}
+
