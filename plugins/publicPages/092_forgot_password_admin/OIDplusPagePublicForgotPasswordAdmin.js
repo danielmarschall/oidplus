7b64a7d69d5704d1fc68614b2752ffaf8b6b6821
Daniel Marschall <info@daniel-marschall.de>
2021-05-24 22:44:22 +0000

    Fixed bcrypt worker
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@599 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/publicPages/092_forgot_password_admin/OIDplusPagePublicForgotPasswordAdmin.js
+++ plugins/publicPages/092_forgot_password_admin/OIDplusPagePublicForgotPasswordAdmin.js
@@ -66,7 +66,7 @@ var OIDplusPagePublicForgotPasswordAdmin = {
 					g_prevBcryptPw = null;
 					bCryptWorker.terminate();
 				}
-				bCryptWorker = new Worker('includes/bcrypt_worker.js');
+				bCryptWorker = new Worker('bcrypt_worker.js');
 				bCryptWorker.postMessage(pw);
 				bCryptWorker.onmessage = function (event) {
 					var admPwdHash = event.data;
