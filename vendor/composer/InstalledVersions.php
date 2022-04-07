ab8114705e03e42e75970d611b04a983625fefb6
Daniel Marschall <info@daniel-marschall.de>
2022-04-07 22:57:07 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@823 02e4d621-2042-4998-a0bc-9ccd24201011

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -102,7 +102,7 @@ private static $installed = array (
       array (
         0 => '9999999-dev',
       ),
-      'reference' => '117090c03ad95c35f699aacd8750bbaeef3d8268',
+      'reference' => 'c09f15bab42be64809d29d0409b4fa2098fefa44',
     ),
     'dcodeio/bcrypt.js' => 
     array (
