f15d524455833628d0c284fb3d069396e93614f1
Daniel Marschall <info@daniel-marschall.de>
2022-05-30 21:21:04 +0000

    Fixed phpstan warning
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@861 02e4d621-2042-4998-a0bc-9ccd24201011

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,11 +65,16 @@ class ComposerAutoloaderInitOidPlusComposer
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequireOidPlusComposer($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
