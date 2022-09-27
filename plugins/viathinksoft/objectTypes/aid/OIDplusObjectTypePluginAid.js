051162d742f2458745ec4a32993c3acee31ff4c1
Daniel Marschall <info@daniel-marschall.de>
2022-09-27 21:24:26 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@966 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/viathinksoft/objectTypes/aid/OIDplusObjectTypePluginAid.js
+++ plugins/viathinksoft/objectTypes/aid/OIDplusObjectTypePluginAid.js
@@ -20,13 +20,21 @@ var OIDplusObjectTypePluginAid = {
 	OID: "1.3.6.1.4.1.37476.2.5.2.4.8.11",
 
 	generateRandomAID: function() {
-		var length           = 32-1;
-		var result           = 'F'; // category "F" = Unregistered Proprietary AID
-		var characters       = '0123456789ABCDEF';
-		var charactersLength = characters.length;
-		for ( var i = 0; i < length; i++ ) {
-			result += characters.charAt(Math.floor(Math.random() * charactersLength));
-		}
+		var length     = 32; // 32 nibbles = 16 bytes
+		var characters = '0123456789ABCDEF';
+		do {
+			var result = 'F'; // category "F" = Unregistered Proprietary AID
+			for ( var i = 0 ; i < length - 1/*F*/ ; i++ ) {
+				result += characters.charAt(Math.floor(Math.random() * characters.length));
+			}
+		} while (result.endsWith('FF') && (result.length==32));
+
+		// Note that 16 byte AIDs ending with 0xFF *were* reserved by ISO in ISO 7816-4:1994,
+		// but modern versions of ISO 7816-4 and ISO 7816-4 do not mention this case anymore.
+		// It can be assumed that the usage is safe, but just to be sure, we exclude 16-byte
+		// AIDs ending with 0xFF, in case there are some software implementations which
+		// deny such AIDs.
+
 		$("#id").val(result);
 	}
 
