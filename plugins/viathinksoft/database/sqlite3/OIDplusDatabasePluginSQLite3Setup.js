c88321e7757e47d6f161bdc4e283561d132b3feb
Daniel Marschall <info@daniel-marschall.de>
2022-10-03 22:16:46 +0000

    Setup DB command lines: Added "copy to clipboard" buttons
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@985 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/viathinksoft/database/sqlite3/OIDplusDatabasePluginSQLite3Setup.js
+++ plugins/viathinksoft/database/sqlite3/OIDplusDatabasePluginSQLite3Setup.js
@@ -43,8 +43,8 @@ rebuild_callbacks.push(function() {
 
 	$("#struct_1")[0].href = setupdir+'struct_empty.sql.php?plugin=sqlite&prefix='+encodeURI($("#tablename_prefix")[0].value)+'&slang=sqlite';
 	$("#struct_2")[0].href = setupdir+'struct_with_examples.sql.php?plugin=sqlite&prefix='+encodeURI($("#tablename_prefix")[0].value)+'&slang=sqlite';
-	$("#struct_cli_1")[0].innerHTML = '<br>'+_L('or via command line:')+'<br><code>curl -s "'+$("#struct_1")[0].href+'" | sqlite3 '+$("#sqlite3_file")[0].value+'<!-- TODO: encryption key? --></code>';
-	$("#struct_cli_2")[0].innerHTML = '<br>'+_L('or via command line:')+'<br><code>curl -s "'+$("#struct_2")[0].href+'" | sqlite3 '+$("#sqlite3_file")[0].value+'<!-- TODO: encryption key? --></code>';
+	$("#struct_cli_1")[0].innerHTML = '<br>'+_L('or via command line:')+'<br><code id="struct_cli_1_code">curl -s "'+$("#struct_1")[0].href+'" | sqlite3 '+$("#sqlite3_file")[0].value+'<!-- TODO: encryption key? --></code><br><input type="button" value="'+_L('Copy to clipboard')+'" onClick="copyToClipboard(struct_cli_1_code)">';
+	$("#struct_cli_2")[0].innerHTML = '<br>'+_L('or via command line:')+'<br><code id="struct_cli_2_code">curl -s "'+$("#struct_2")[0].href+'" | sqlite3 '+$("#sqlite3_file")[0].value+'<!-- TODO: encryption key? --></code><br><input type="button" value="'+_L('Copy to clipboard')+'" onClick="copyToClipboard(struct_cli_2_code)">';
 
 	return !error;
 });
