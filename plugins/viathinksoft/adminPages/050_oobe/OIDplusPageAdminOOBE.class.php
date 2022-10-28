82efbb8d3a707982ca5e30c87cd9361fc52c95c7
Daniel Marschall <info@daniel-marschall.de>
2022-10-28 08:11:40 +0000

    Fixed bug in System Registration request after OOBE
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1029 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/viathinksoft/adminPages/050_oobe/OIDplusPageAdminOOBE.class.php
+++ plugins/viathinksoft/adminPages/050_oobe/OIDplusPageAdminOOBE.class.php
@@ -41,22 +41,34 @@ class OIDplusPageAdminOOBE extends OIDplusPagePluginAdmin {
 	}
 
 	public function init($html=true) {
-		OIDplus::config()->delete('reg_wizard_done');
+		OIDplus::config()->delete('reg_wizard_done'); // deprecated name
 		OIDplus::config()->prepareConfigKey('oobe_main_done', '"Out Of Box Experience" wizard for the system settings done once?', '0', OIDplusConfig::PROTECTION_HIDDEN, function($value) {});
 
-		// In the OOBE, "get_challenge" of the ViaThinkSoft Captcha will raise the error:
-		// "A plugin has requested that the initialization wizard (OOBE) is shown. Please reload the page."
-		// So we must not continue if the referrer is OOBE.
-		if (isset($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'],'050_oobe/oobe.php') !== false)) return;
-
 		if ($this->oobeRequired()) {
-			// Show registration/configuration wizard once
 			if ($html) {
+				// Show registration/configuration wizard once
 				if (basename($_SERVER['SCRIPT_NAME']) != 'oobe.php') {
 					header('Location:'.OIDplus::webpath(__DIR__,OIDplus::PATH_RELATIVE).'oobe.php');
 					die(_L('Redirecting to Out-Of-Box-Experience wizard...'));
 				}
 			} else {
+				// In the OOBE, "get_challenge" of the ViaThinkSoft Captcha will raise the error:
+				// "A plugin has requested that the initialization wizard (OOBE) is shown. Please reload the page."
+				// So we must not continue if the referrer is OOBE.
+				if (isset($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'],'050_oobe/oobe.php') !== false)) return;
+
+				// This is another very special case...
+				// When the Registration OOBE is saved, the 'reg_privacy" setting will be
+				// set, which will call the ViaThinkSoft server. The ViaThinkSoft server
+				// then calls the "verify pubkey" action, but this fails because
+				// the system is still in OOBE mode!
+				if (basename($_SERVER['SCRIPT_NAME']) == 'ajax.php') {
+					$req = array_merge($_POST,$_GET);
+					if (($req['plugin'] == '1.3.6.1.4.1.37476.2.5.2.4.3.120') && ($req['action'] = 'verify_pubkey')) {
+						return;
+					}
+				}
+
 				// We cannot guarantee that everything works correctly if OOBE never ran once. So abort AJAX and co.
 				throw new OIDplusException(_L('A plugin has requested that the initialization wizard (OOBE) is shown. Please reload the page.'));
 			}
