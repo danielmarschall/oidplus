3ea811b3202b9f3eac65713d9fdad62762f52c05
Daniel Marschall <info@daniel-marschall.de>
2022-11-27 01:15:58 +0000

    Vendor update
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1037 02e4d621-2042-4998-a0bc-9ccd24201011

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -221,9 +221,9 @@
             'dev_requirement' => false,
         ),
         'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.2.2',
-            'version' => '5.2.2.0',
-            'reference' => '961d5ff9844372a4e294980c667bbe7e0651cdeb',
+            'pretty_version' => 'v5.2.3',
+            'version' => '5.2.3.0',
+            'reference' => 'cb021439c683d9805e2864c58095b92d405e9b11',
             'type' => 'library',
             'install_path' => __DIR__ . '/../twbs/bootstrap',
             'aliases' => array(),
@@ -241,7 +241,7 @@
         'twitter/bootstrap' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => 'v5.2.2',
+                0 => 'v5.2.3',
             ),
         ),
         'vakata/jstree' => array(
