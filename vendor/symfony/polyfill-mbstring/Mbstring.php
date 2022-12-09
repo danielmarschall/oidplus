8ef762bd076f92674be01646620d231261a7d28d
Daniel Marschall <info@daniel-marschall.de>
2022-12-09 21:05:45 +0000

    PHP 8.2.0 compatibility
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1042 02e4d621-2042-4998-a0bc-9ccd24201011

--- vendor/symfony/polyfill-mbstring/Mbstring.php
+++ vendor/symfony/polyfill-mbstring/Mbstring.php
@@ -79,7 +79,9 @@ final class Mbstring
 
     public static function mb_convert_encoding($s, $toEncoding, $fromEncoding = null)
     {
-        if (\is_array($fromEncoding) || false !== strpos($fromEncoding, ',')) {
+        // Daniel Marschall Hotfix 09 Dec 2022 to support PHP 8.2
+        //if (\is_array($fromEncoding) || false !== strpos($fromEncoding, ',')) {
+        if (!is_null($fromEncoding) && ((\is_array($fromEncoding) || false !== strpos($fromEncoding, ',')))) {
             $fromEncoding = self::mb_detect_encoding($s, $fromEncoding);
         } else {
             $fromEncoding = self::getEncoding($fromEncoding);
