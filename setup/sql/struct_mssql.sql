3f9a08ae22d62752acb4bcc51a32b005d6ea51db
Daniel Marschall <info@daniel-marschall.de>
2023-04-18 09:23:16 +0000

    SQL Server is now Unicode and emoji compatible. Existing databases need to change [text] to [ntext] and [varchar] to [nvarchar]
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1217 02e4d621-2042-4998-a0bc-9ccd24201011

--- setup/sql/struct_mssql.sql
+++ setup/sql/struct_mssql.sql
@@ -12,9 +12,9 @@ GO
 IF OBJECT_ID('dbo.config', 'U') IS NOT NULL /*Backwards compatibility*/
 	DROP TABLE /*IF EXISTS*/ [dbo].[config];
 CREATE TABLE [dbo].[config](
-	[name] [varchar](50) NOT NULL,
-	[value] [text] NOT NULL,
-	[description] [varchar](255) NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[value] [ntext] NOT NULL,
+	[description] [nvarchar](255) NULL,
 	[protected] [bit] NOT NULL DEFAULT ('0'),
 	[visible] [bit] NOT NULL DEFAULT ('0'),
 	CONSTRAINT [PK_config] PRIMARY KEY CLUSTERED 
@@ -30,8 +30,8 @@ IF OBJECT_ID('dbo.asn1id', 'U') IS NOT NULL /*Backwards compatibility*/
 	DROP TABLE /*IF EXISTS*/ [dbo].[asn1id];
 CREATE TABLE [dbo].[asn1id](
 	[lfd] [int] IDENTITY(1,1) NOT NULL,
-	[oid] [varchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
-	[name] [varchar](255) NOT NULL,
+	[oid] [nvarchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
 	[standardized] [bit] NOT NULL CONSTRAINT [DF__asn1id__standardized]  DEFAULT ('0'),
 	[well_known] [bit] NOT NULL CONSTRAINT [DF__asn1id__well_known]  DEFAULT ('0'),
  	CONSTRAINT [PK_asn1id] PRIMARY KEY CLUSTERED 
@@ -52,8 +52,8 @@ IF OBJECT_ID('dbo.iri', 'U') IS NOT NULL /*Backwards compatibility*/
 	DROP TABLE /*IF EXISTS*/ [dbo].[iri];
 CREATE TABLE [dbo].[iri](
 	[lfd] [int] IDENTITY(1,1) NOT NULL,
-	[oid] [varchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
-	[name] [varchar](255) NOT NULL,
+	[oid] [nvarchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
 	[longarc] [bit] NOT NULL CONSTRAINT [DF__iri__longarc]  DEFAULT ('0'),
 	[well_known] [bit] NOT NULL CONSTRAINT [DF__iri__well_known]  DEFAULT ('0'),
 	CONSTRAINT [PK_iri] PRIMARY KEY CLUSTERED 
@@ -73,15 +73,15 @@ GO
 IF OBJECT_ID('dbo.objects', 'U') IS NOT NULL /*Backwards compatibility*/
 	DROP TABLE /*IF EXISTS*/ [dbo].[objects];
 CREATE TABLE [dbo].[objects](
-	[id] [varchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
-	[parent] [varchar](255) COLLATE German_PhoneBook_CS_AS NULL,
-	[title] [varchar](255) NULL,
-	[description] [text] NULL,
-	[ra_email] [varchar](100) NULL,
+	[id] [nvarchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
+	[parent] [nvarchar](255) COLLATE German_PhoneBook_CS_AS NULL,
+	[title] [nvarchar](255) NULL,
+	[description] [ntext] NULL,
+	[ra_email] [nvarchar](100) NULL,
 	[confidential] [bit] NOT NULL,
 	[created] [datetime] NULL,
 	[updated] [datetime] NULL,
-	[comment] [varchar](255) NULL,
+	[comment] [nvarchar](255) NULL,
 	CONSTRAINT [PK_objects] PRIMARY KEY CLUSTERED 
 	(
 		[id] ASC
@@ -103,19 +103,19 @@ IF OBJECT_ID('dbo.ra', 'U') IS NOT NULL /*Backwards compatibility*/
 	DROP TABLE /*IF EXISTS*/ [dbo].[ra];
 CREATE TABLE [dbo].[ra](
 	[ra_id] [int] IDENTITY(1,1) NOT NULL,
-	[email] [varchar](100) NOT NULL,
-	[ra_name] [varchar](100) NULL,
-	[personal_name] [varchar](100) NULL,
-	[organization] [varchar](100) NULL,
-	[office] [varchar](100) NULL,
-	[street] [varchar](100) NULL,
-	[zip_town] [varchar](100) NULL,
-	[country] [varchar](100) NULL,
-	[phone] [varchar](100) NULL,
-	[mobile] [varchar](100) NULL,
-	[fax] [varchar](100) NULL,
+	[email] [nvarchar](100) NOT NULL,
+	[ra_name] [nvarchar](100) NULL,
+	[personal_name] [nvarchar](100) NULL,
+	[organization] [nvarchar](100) NULL,
+	[office] [nvarchar](100) NULL,
+	[street] [nvarchar](100) NULL,
+	[zip_town] [nvarchar](100) NULL,
+	[country] [nvarchar](100) NULL,
+	[phone] [nvarchar](100) NULL,
+	[mobile] [nvarchar](100) NULL,
+	[fax] [nvarchar](100) NULL,
 	[privacy] [bit] NOT NULL CONSTRAINT [DF__ra__privacy]  DEFAULT ('0'),
-	[authkey] [varchar](250) NULL,
+	[authkey] [nvarchar](250) NULL,
 	[registered] [datetime] NULL,
 	[updated] [datetime] NULL,
 	[last_login] [datetime] NULL,
@@ -136,8 +136,8 @@ IF OBJECT_ID('dbo.log', 'U') IS NOT NULL /*Backwards compatibility*/
 CREATE TABLE [dbo].[log](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[unix_ts] [bigint] NOT NULL,
-	[addr] [varchar](45) NOT NULL,
-	[event] [text] NOT NULL,
+	[addr] [nvarchar](45) NOT NULL,
+	[event] [ntext] NOT NULL,
 	CONSTRAINT [PK_log] PRIMARY KEY CLUSTERED 
 	(
 		[id] ASC
@@ -152,7 +152,7 @@ IF OBJECT_ID('dbo.log_user', 'U') IS NOT NULL /*Backwards compatibility*/
 CREATE TABLE [dbo].[log_user](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[log_id] [int] NOT NULL,
-	[username] [varchar](255) NOT NULL,
+	[username] [nvarchar](255) NOT NULL,
 	[severity] [int] NOT NULL,
 	CONSTRAINT [PK_log_user] PRIMARY KEY CLUSTERED 
 	(
@@ -181,7 +181,7 @@ IF OBJECT_ID('dbo.log_object', 'U') IS NOT NULL /*Backwards compatibility*/
 CREATE TABLE [dbo].[log_object](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[log_id] [int] NOT NULL,
-	[object] [varchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
+	[object] [nvarchar](255) COLLATE German_PhoneBook_CS_AS NOT NULL,
 	[severity] [int] NOT NULL,
 	CONSTRAINT [PK_log_object] PRIMARY KEY CLUSTERED 
 	(
