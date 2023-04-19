3e3c9e62e5b2b6a861013c11ab75c7ab51738d31
Daniel Marschall <info@daniel-marschall.de>
2023-04-19 18:32:19 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1224 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/viathinksoft/logger/000_database/OIDplusLoggerPluginDatabase.class.php
+++ plugins/viathinksoft/logger/000_database/OIDplusLoggerPluginDatabase.class.php
@@ -44,11 +44,8 @@ class OIDplusLoggerPluginDatabase extends OIDplusLoggerPlugin {
 		OIDplus::dbIsolated()->query("insert into ###log (addr, unix_ts, event) values (?, ?, ?)", array($addr, time(), $event->getMessage())); // TODO: why unix_ts? Why not a database DATETIME field?!
 		$log_id = OIDplus::dbIsolated()->insert_id();
 		if ($log_id === 0) {
-			$res = OIDplus::dbIsolated()->query("select max(id) as last_id from ###log");
-			if (!$res->any()) throw new OIDplusException(_L('Could not log event'));
-			$row = $res->fetch_array();
-			$log_id = $row['last_id'];
-			if ($log_id == 0) throw new OIDplusException(_L('Could not log event'));
+			$log_id = OIDplus::dbIsolated()->getScalar("select max(id) as last_id from ###log");
+			if (!$log_id) throw new OIDplusException(_L('Could not log event'));
 		}
 
 		$object_dupe_check = array();
