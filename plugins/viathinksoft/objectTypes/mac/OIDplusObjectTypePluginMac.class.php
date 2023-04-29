2bb720c934d475c9a12064dde24c34ba3685ff1f
Daniel Marschall <info@daniel-marschall.de>
2023-04-29 22:10:35 +0000

    New object type: MAC / EUI-48 / EUI-64
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1248 02e4d621-2042-4998-a0bc-9ccd24201011

---
+++ plugins/viathinksoft/objectTypes/mac/OIDplusObjectTypePluginMac.class.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * OIDplus 2.0
+ * Copyright 2019 - 2023 Daniel Marschall, ViaThinkSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace ViaThinkSoft\OIDplus;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('INSIDE_OIDPLUS') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+class OIDplusObjectTypePluginMac extends OIDplusObjectTypePlugin {
+
+	/**
+	 * @return string
+	 */
+	public static function getObjectTypeClassName(): string {
+		return OIDplusMac::class;
+	}
+
+	/**
+	 * @param string $static_node_id
+	 * @param bool $throw_exception
+	 * @return string
+	 */
+	public static function prefilterQuery(string $static_node_id, bool $throw_exception): string {
+		$static_node_id = preg_replace('@^eui:@', 'mac:', $static_node_id);
+
+		if (str_starts_with($static_node_id,'mac:')) {
+			$static_node_id = str_replace(' ', '', $static_node_id);
+		}
+
+		// Special treatment for OIDs: if someone enters a valid MAC address in the goto box, prepend "mac:"
+		if ((str_contains($static_node_id, ':') || str_contains($static_node_id, '-')) && mac_valid($static_node_id)) {
+			$static_node_id = 'mac:'.$static_node_id;
+		}
+
+		return $static_node_id;
+	}
+
+}
