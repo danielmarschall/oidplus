3d74e0574cbaff8d287d304dd82a810b7a1ee4a5
Daniel Marschall <info@daniel-marschall.de>
2023-09-25 09:14:31 +0000

    Fixed PHP error in OIDplusPagePublicRaInfo.class.php
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1409 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/viathinksoft/publicPages/093_rainfo/OIDplusPagePublicRaInfo.class.php
+++ plugins/viathinksoft/publicPages/093_rainfo/OIDplusPagePublicRaInfo.class.php
@@ -149,7 +149,7 @@ class OIDplusPagePublicRaInfo extends OIDplusPagePluginPublic {
 	 */
 	public function publicSitemap(array &$out) {
 		if (OIDplus::db()->getSlang()->id() == 'mysql') {
-			$res = OIDplus::db()->query("select distinct BINARY(email) as email from ###ra"); // "binary" because we want to ensure that 'distinct' is case sensitive
+			$res = OIDplus::db()->query("select distinct BINARY(email) as distinct_email from ###ra"); // "binary" because we want to ensure that 'distinct' is case sensitive
 		} else {
 			$res = OIDplus::db()->query("select distinct email as distinct_email from ###ra"); // distinct in PGSQL is always case sensitive
 		}
