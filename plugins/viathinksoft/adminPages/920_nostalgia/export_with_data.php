912cd3d12ffcfc8edbf00049cf78cab508a00f94
Daniel Marschall <info@daniel-marschall.de>
2024-07-29 00:16:39 +0000

    Nostalgia Plugin now creates only DOS files which can also be read by the Win311/Win95 binaries
    git-svn-id: https://svn.viathinksoft.com/svn/oidplus/trunk@1527 02e4d621-2042-4998-a0bc-9ccd24201011

--- plugins/viathinksoft/adminPages/920_nostalgia/export_with_data.php
+++ plugins/viathinksoft/adminPages/920_nostalgia/export_with_data.php
@@ -2,7 +2,7 @@
 
 /*
  * OIDplus 2.0
- * Copyright 2019 - 2023 Daniel Marschall, ViaThinkSoft
+ * Copyright 2019 - 2024 Daniel Marschall, ViaThinkSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,68 @@ if (!class_exists('ZipArchive')) {
 	throw new OIDplusException(_L('The PHP extension "ZipArchive" needs to be installed to create a ZIP archive with an included database. Otherwise, you can just download the plain program without data.'));
 }
 
+$tmp_file = OIDplus::getUserDataDir("cache").'oidplus_ancient.zip';
+
+$zip = new ZipArchive();
+if ($zip->open($tmp_file, ZipArchive::CREATE)!== true) {
+	throw new OIDplusException(_L("Cannot open file %1", $tmp_file));
+}
+
+// ---------------------------- RA
+
+// Now check all RAs
+$i = 0;
+$res = OIDplus::db()->query("select * from ###ra");
+$res->naturalSortByField('email');
+$ra_dos_ids = [];
+while ($row = $res->fetch_object()) {
+	$ra_dos_ids[$row->email] = str_pad(strval($i++), 8, '0', STR_PAD_LEFT);
+	$ra_name[$row->email] = $row->ra_name;
+	$ra_phone[$row->email] = $row->phone;
+	$ra_cdat[$row->email] = fix_datetime_for_output($row->registered);
+	$ra_udat[$row->email] = fix_datetime_for_output($row->updated);
+}
+
+// https://github.com/danielmarschall/oidplus_win311/blob/master/RAFILE.PAS
+// https://github.com/danielmarschall/oidplus_win95/blob/master/RAFILE.PAS
+
+const RA_CMD_VERSION          = 'VERS';
+const RA_CMD_NAME             = 'NAME';
+const RA_CMD_EMAIL            = 'MAIL';
+const RA_CMD_PHONE            = 'PHON';
+const RA_CMD_CREATE_DATE      = 'CDAT';
+const RA_CMD_UPDATE_DATE      = 'UDAT';
+
+$idxfile = '';
+
+foreach ($ra_dos_ids as $ra_email => $dos_id) {
+	$cont = make_line(RA_CMD_VERSION, '2024');
+
+	$cont .= make_line(RA_CMD_NAME, $ra_name[$ra_email]);
+
+	$cont .= make_line(RA_CMD_EMAIL, $ra_email);
+
+	$cont .= make_line(RA_CMD_PHONE, $ra_phone[$ra_email]);
+
+	$cont .= make_line(RA_CMD_CREATE_DATE, $ra_cdat[$ra_email] ?? '1900-01-01');
+
+	$cont .= make_line(RA_CMD_UPDATE_DATE, $ra_udat[$ra_email] ?? '1900-01-01');
+
+	//echo "****$dos_id.RA_\r\n";
+	//echo "$cont\r\n";
+
+	$zip->addFromString("$dos_id.RA_", $cont);
+
+	$idxfile .= make_line($dos_id, $ra_email);
+}
+
+//echo "****00000000.RA_\r\n";
+//echo "$idxfile\r\n";
+
+$zip->addFromString("00000000.RA_", $idxfile);
+
+// ---------------------------- OIDS
+
 $dos_ids = array();
 $parent_oids = array();
 $i = 0;
@@ -55,6 +117,9 @@ $iri[''] = array();
 $asn1[''] = array();
 $title[''] = 'OID Root';
 $description[''] = 'Exported by OIDplus 2.0';
+$cdat[''] = '1900-01-01';
+$udat[''] = '1900-01-01';
+$ra[''] = '';
 
 // Now check all OIDs
 $res = OIDplus::db()->query("select * from ###objects where id like 'oid:%'");
@@ -68,6 +133,9 @@ while ($row = $res->fetch_object()) {
 	fill_iri($oid, $iri);
 	$title[$oid] = vts_utf8_decode($row->title);
 	$description[$oid] = vts_utf8_decode($row->description);
+	$cdat[$oid] = fix_datetime_for_output($row->created);
+	$udat[$oid] = fix_datetime_for_output($row->updated);
+	$ra[$oid] = $row->ra_email;
 
 	if ((oid_len($oid) > 1) && ($parent_oid == '')) {
 		do {
@@ -81,10 +149,17 @@ while ($row = $res->fetch_object()) {
 			fill_iri($real_parent, $iri); // well-known OIDs?
 			$title[$real_parent] = '';
 			$description[$real_parent] = '';
+			$cdat[$real_parent] = '';
+			$udat[$real_parent] = '';
+			$ra[$real_parent] = '';
+
 			$res2 = OIDplus::db()->query("select * from ###objects where id = ?", ["oid:$real_parent"]);
 			while ($row2 = $res2->fetch_object()) {
 				$title[$real_parent] = vts_utf8_decode($row2->title);
 				$description[$real_parent] = vts_utf8_decode($row2->description);
+				$cdat[$real_parent] = fix_datetime_for_output($row2->created);
+				$udat[$real_parent] = fix_datetime_for_output($row2->updated);
+				$ra[$real_parent] = $row2->ra_email;
 			}
 
 			// next
@@ -96,54 +171,44 @@ while ($row = $res->fetch_object()) {
 	}
 }
 
-$tmp_file = OIDplus::getUserDataDir("cache").'dos_export.zip';
-
-$zip = new ZipArchive();
-if ($zip->open($tmp_file, ZipArchive::CREATE)!== true) {
-	throw new OIDplusException(_L("Cannot open file %1", $tmp_file));
-}
-
-/**
- * @param string $command
- * @param string $data
- * @return string
- */
-function make_line(string $command, string $data): string {
-	return $command.$data."\r\n";
-}
-
 // https://github.com/danielmarschall/oidplus_dos/blob/master/OIDFILE.PAS
-const CMD_VERSION         = 'VERS';
-const CMD_OWN_ID          = 'SELF';
-const CMD_PARENT          = 'SUPR';
-const CMD_CHILD           = 'CHLD';
-const CMD_ASN1_IDENTIFIER = 'ASN1';
-const CMD_UNICODE_LABEL   = 'UNIL';
-const CMD_DESCRIPTION     = 'DESC';
+// https://github.com/danielmarschall/oidplus_win311/blob/master/OIDFILE.PAS
+// https://github.com/danielmarschall/oidplus_win95/blob/master/OIDFILE.PAS
+const OID_CMD_VERSION         = 'VERS';
+const OID_CMD_OWN_ID          = 'SELF';
+const OID_CMD_PARENT          = 'SUPR';
+const OID_CMD_CHILD           = 'CHLD';
+const OID_CMD_ASN1_IDENTIFIER = 'ASN1';
+const OID_CMD_UNICODE_LABEL   = 'UNIL';
+const OID_CMD_DESCRIPTION     = 'DESC';
+const OID_CMD_CREATE_DATE     = 'CDAT';
+const OID_CMD_UPDATE_DATE     = 'UDAT';
+const OID_CMD_DRAFT           = 'DRFT';
+const OID_CMD_RA              = 'RA__';
 
 foreach ($dos_ids as $oid => $dos_id) {
-	$cont = make_line(CMD_VERSION, '2022');
+	$cont = make_line(OID_CMD_VERSION, '2022');
 
-	$cont .= make_line(CMD_OWN_ID, $dos_id.$oid);
+	$cont .= make_line(OID_CMD_OWN_ID, $dos_id.$oid);
 
 	$parent_oid = $parent_oids[$oid];
 	$parent_id = $dos_ids[$parent_oid];
-	$cont .= make_line(CMD_PARENT, $parent_id.$parent_oid);
+	$cont .= make_line(OID_CMD_PARENT, $parent_id.$parent_oid);
 
 	foreach ($parent_oids as $child_oid => $parent_oid) {
 		if ($child_oid == '') continue;
 		if ($parent_oid == $oid) {
 			$child_id = $dos_ids[$child_oid];
-			$cont .= make_line(CMD_CHILD, $child_id.$child_oid);
+			$cont .= make_line(OID_CMD_CHILD, $child_id.$child_oid);
 		}
 	}
 
 	foreach ($asn1[$oid] as $name) {
-		$cont .= make_line(CMD_ASN1_IDENTIFIER, $name);
+		$cont .= make_line(OID_CMD_ASN1_IDENTIFIER, $name);
 	}
 
 	foreach ($iri[$oid] as $name) {
-		$cont .= make_line(CMD_UNICODE_LABEL, $name);
+		$cont .= make_line(OID_CMD_UNICODE_LABEL, $name);
 	}
 
 	$desc_ary1 = handleDesc_dos($title[$oid]);
@@ -152,10 +217,18 @@ foreach ($dos_ids as $oid => $dos_id) {
 	$prev_line = '';
 	foreach ($desc_ary as $line_idx => $line) {
 		if ($line == $prev_line) continue;
-		if ($line_idx >= 10/*DESCEDIT_LINES*/) break;
-		$cont .= make_line(CMD_DESCRIPTION, $line);
+		//if ($line_idx >= 10/*DESCEDIT_LINES*/) break;
+		$cont .= make_line(OID_CMD_DESCRIPTION, $line);
 		$prev_line = $line;
 	}
+
+	$cont .= make_line(OID_CMD_CREATE_DATE, $cdat[$oid] ?? '1900-01-01');
+
+	$cont .= make_line(OID_CMD_UPDATE_DATE, $udat[$oid] ?? '1900-01-01');
+
+	$cont .= make_line(OID_CMD_DRAFT, '0'); // this attribute does not exist in OIDplus 2.0
+
+	$cont .= make_line(OID_CMD_RA, $ra[$oid]);
 
 	//echo "****$dos_id.OID\r\n";
 	//echo "$cont\r\n";
@@ -163,18 +236,34 @@ foreach ($dos_ids as $oid => $dos_id) {
 	$zip->addFromString("$dos_id.OID", $cont);
 }
 
+// ---------------------------- EXE
+
 $exe_url = 'https://github.com/danielmarschall/oidplus_dos/raw/master/OIDPLUS.EXE';
 $exe = url_get_contents($exe_url);
 if ($exe === false) {
 	throw new OIDplusException(_L("Cannot download the binary file from GitHub (%1)", $exe_url));
 }
-$zip->addFromString('OIDPLUS.EXE', $exe);
+$zip->addFromString('OIDDBDOS.EXE', $exe);
+
+$exe_url = 'https://github.com/danielmarschall/oidplus_win95/raw/master/OIDPLUS.exe';
+$exe = url_get_contents($exe_url);
+if ($exe === false) {
+	throw new OIDplusException(_L("Cannot download the binary file from GitHub (%1)", $exe_url));
+}
+$zip->addFromString('OIDDB_32.EXE', $exe);
+
+$exe_url = 'https://github.com/danielmarschall/oidplus_win311/raw/master/OIDPLUS.exe';
+$exe = url_get_contents($exe_url);
+if ($exe === false) {
+	throw new OIDplusException(_L("Cannot download the binary file from GitHub (%1)", $exe_url));
+}
+$zip->addFromString('OIDDB_16.EXE', $exe);
 
 $zip->close();
 
 if (!headers_sent()) {
 	header('Content-Type: application/zip');
-	header('Content-Disposition: attachment; filename=oidplus_dos.zip');
+	header('Content-Disposition: attachment; filename=oidplus_ancient.zip');
 	readfile($tmp_file);
 }
 
@@ -190,7 +279,7 @@ OIDplus::invoke_shutdown();
  * @return void
  * @throws OIDplusException
  */
-function fill_asn1(string $oid, array &$asn1) {
+function fill_asn1(string $oid, array &$asn1): void {
 	if (!isset($asn1[$oid])) $asn1[$oid] = array();
 	$res = OIDplus::db()->query("select * from ###asn1id where oid = ?", ["oid:$oid"]);
 	while ($row = $res->fetch_object()) {
@@ -204,7 +293,7 @@ function fill_asn1(string $oid, array &$asn1) {
  * @return void
  * @throws OIDplusException
  */
-function fill_iri(string $oid, array &$iri) {
+function fill_iri(string $oid, array &$iri): void {
 	if (!isset($iri[$oid])) $iri[$oid] = array();
 	$res = OIDplus::db()->query("select * from ###iri where oid = ?", ["oid:$oid"]);
 	while ($row = $res->fetch_object()) {
@@ -228,4 +317,34 @@ function handleDesc_dos(string $desc): array {
 	$desc_ary = explode("\r\n", wordwrap($desc, 75, "\r\n", true));
 	if (implode('',$desc_ary) == '') $desc_ary = array();
 	return $desc_ary;
+}
+
+/**
+ * @param mixed|string|null $datetime
+ * @return mixed|string|null
+ */
+function fix_datetime_for_output($datetime) {
+	if ($datetime === "0000-00-00") $datetime = null; // MySQL might use this as default instead of NULL... But SQL Server cannot read this.
+
+	if (is_string($datetime) && (substr($datetime,4,1) !== '-')) {
+		// Let's hope PHP can convert the database language specific string to ymd
+		$time = @strtotime($datetime);
+		if ($time) {
+			$date = date('Y-m-d', $time);
+			if ($date) {
+				$datetime = $date;
+			}
+		}
+	}
+
+	return explode(' ', $datetime)[0]; // only date, not time
+}
+
+/**
+ * @param string $command
+ * @param string $data
+ * @return string
+ */
+function make_line(string $command, string $data): string {
+	return $command.$data."\r\n";
 }
